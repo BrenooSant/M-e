86d76a58d01f2cc15402f1eb3c9af6f22ca12126
Breno Santos <brenoosant19@gmail.com>
2025-05-11 10:40:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const envelope = document.getElementById("envelope");
+const envelope = document.querySelector('.envelope');
 
-  envelope.addEventListener("click", function () {
-    envelope.classList.toggle("open");
+  envelope.addEventListener('click', () => {
+    envelope.classList.toggle('open');
   });
-});
+
